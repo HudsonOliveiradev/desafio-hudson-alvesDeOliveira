ea2ea6dd4e13edf31ab7d3278aed520a39c33191
Hudson Alves de Oliveira <hudsonbiomed15@gmail.com>
2023-08-16 16:46:44 -0300

    Proposta final do desafio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,58 +1,81 @@
 class CaixaDaLanchonete {
-
     cardapio = [
-{codigo: cafe, nome: "Café", valorDoProduto: 3.00},
-{codigo: chantily, nome: "Chantily (extra do Café)", valorDoProduto: 1.50},
-{codigo: suco, nome: "Suco Natural", valorDoProduto: 6.20},
-{codigo: sanduiche, nome: "Sanduíche", valorDoProduto: 6.50},
-{codigo: queijo, nome: "Queijo (extra do Sanduíche)", valorDoProduto: 2.00},
-{codigo: salgado, nome: "Salgado", valorDoProduto: 7.25},
-{codigo: combo1, nome: "1 Suco e 1 Sanduíche" , valorDoProduto: 9.50},
-{codigo: combo2, nome: "1 Café e 1 Sanduíche", valorDoProduto: 7.50}
-];  
+        { codigo: 'cafe', nome: "Café", valorDoProduto: 3.00 },
+        { codigo: 'chantily', nome: "Chantily (extra do Café)", valorDoProduto: 1.50 },
+        { codigo: 'suco', nome: "Suco Natural", valorDoProduto: 6.20 },
+        { codigo: 'sanduiche', nome: "Sanduíche", valorDoProduto: 6.50 },
+        { codigo: 'queijo', nome: "Queijo (extra do Sanduíche)", valorDoProduto: 2.00 },
+        { codigo: 'salgado', nome: "Salgado", valorDoProduto: 7.25 },
+        { codigo: 'combo1', nome: "1 Suco e 1 Sanduíche", valorDoProduto: 9.50 },
+        { codigo: 'combo2', nome: "1 Café e 1 Sanduíche", valorDoProduto: 7.50 }
+    ];
 
-    formaDePagamento = {'Débito': 1, 'Crédito': 1.03, 'Dinheiro': 0.95
-    };
-    
-    valorDacompra(formaDePagamento, itens) {
-        const pedido = [];
-        let total = 0;
-    
-        for(const itemPedido of itens){
-            const [codigo, quantidade] = itemPedido.trim().split(',');
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        const formasDePagamento = ['dinheiro', 'debito', 'credito'];
 
-      const item = this.cardapio.find(item => item.codigo === codigo);
-
-      if (quantidade <= 0) {
-        return "Quantidade inválida!";
-      }
-  
-      if (!item) {
-        return "Item inválido!";
-      }
-      
-      pedido.push({ ...item, quantidade: parseInt(quantidade) });
-      total += item.valor * parseInt(quantidade);
-    }
-
-    if (pedido.length === 0) {
-      return "Não há itens no carrinho de compra!";
-    }
-
-    if (formaDePagamento !== 'debito' && formaDePagamento !== 'credito' && formaDePagamento !== 'dinheiro') {
-      return "Forma de pagamento inválida!";
-    }
-
-    const descontoTaxa = this.descontosTaxas[formaDePagamento];
-
-    total += total * descontoTaxa;
-
-    return `R$ ${total.toFixed(2).replace('.', ',')}`;
+        if (!formasDePagamento.includes(metodoDePagamento)) {
+            return "Forma de pagamento inválida!";
         }
-    }
 
-    calcularValorDaCompra(metodoDePagamento, itens); {
-        return "";
+        if (itens.length === 0) {
+            return "Não há itens no carrinho de compra!";
+        }
+
+        let total = 0;
+        let isPrincipaisEncontrados = true;
+        let pedidos = [];
+
+        
+
+        for (const item of itens) {
+            const [codigo, quantidade] = item.split(",");
+            const produto = this.cardapio.find(p => p.codigo === codigo);
+            pedidos.push(produto);
+            if (!produto) {
+                return "Item inválido!";
+            }
+
+            const quantidadeInt = parseInt(quantidade, 10);
+
+            if (quantidadeInt === 0) {
+                return "Quantidade inválida!";
+            }
+
+            total += produto.valorDoProduto * quantidadeInt;
+        }
+
+        function verificaValor(obj, valorProcurado) {
+            for (let chave in obj) {
+                if (obj[chave].codigo === valorProcurado) {
+                    return true; // Valor encontrado
+                }
+            }
+            return false; // Valor não encontrado
+        }
+
+        for (let item of pedidos) {
+            if (item.codigo === "chantily") {
+                isPrincipaisEncontrados = verificaValor(pedidos, "cafe");
+            }
+            if (item.codigo === "queijo") {
+                isPrincipaisEncontrados = verificaValor(pedidos, "sanduiche");
+            }
+        }
+
+        if (!isPrincipaisEncontrados) {
+            return "Item extra não pode ser pedido sem o principal";
+        }
+
+        if (metodoDePagamento === 'credito') {
+            total *= 1.03; // 3% de taxa para pagamento no crédito
+        }
+
+        if (metodoDePagamento === 'dinheiro') {
+            total *= 0.95; // 5% de desconto para pagamento em dinheiro
+        }
+        let resultado = `R$ ${total.toFixed(2)}`.replace(".", ",");
+        return resultado;
     }
+}
 
 export { CaixaDaLanchonete };
